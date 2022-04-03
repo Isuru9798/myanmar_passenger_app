149021fca156f130f9d8544aa5dbbe5e29be82c9
Isuru Ruwantha <isuru@techbsl.com>
2022-04-03 13:25:43 +0530

    setup

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFFFF7643);
+const primaryLightColor = Color(0xFFFF7643);
+const primaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFF7643), Color(0xFFFF7643)],
+);
+const secondaryColor = Color(0xFFFF7643);
+const textColor = Color(0xFFFF7643);
+const animationDuration = Duration(milliseconds: 200);
